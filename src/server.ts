7b2517b46f332fc127ff59f4daf8794063891a88
PramisGrg <gurungpramis361@gmail.com>
2024-12-27 19:09:20 +0545

    Initail commit with typescript configuration

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`app running on ${PORT} `);
+});
